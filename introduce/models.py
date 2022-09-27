1855f431607ebe7aab255afa8768a1305c19409f
Sung_cn_2 <112399821+SungChangNam@users.noreply.github.com>
2022-09-27 15:25:18 +0900

    add_Access_Log

--- introduce/models.py
+++ introduce/models.py
@@ -1,3 +1,12 @@
+from re import M
+from unittest.util import _MAX_LENGTH
+from venv import create
 from django.db import models
 
 # Create your models here.
+class AccessLog(models.Model):
+    create_at = models.DateTimeField("접속 시간", auto_now_add=True)
+    locations = models.CharField("접속 경로",max_length=50)
+    
+    def __str__(self):
+        return f"{self.create_at}/ {self.locations} "
--- introduce/views.py
+++ introduce/views.py
@@ -1,5 +1,12 @@
+from os import access
+from re import A
 from django.shortcuts import render
+from introduce.models import AccessLog
 
 # Create your views here.
 def introduce(request):
+    access_log = AccessLog()
+    access_log.locations ="introduce"
+    access_log.save()
+    
     return render(request, 'introduce.html')
